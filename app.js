41177372513d46e3fa1d96429bc52ab5a1f608b9
Mugisha24-pro <bazigajo24@gmail.com>
2025-11-16 23:35:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello MUGISHA, Node.js is working!");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? ", (name) => {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
---
+++ basic.js
@@ -0,0 +1,49 @@
+//Variables in JS
+
+/*let name = "Mugisha";       // String
+let age = 25;               // Number
+let isStudent = true;       // Boolean            
+let hobbies = ["coding", "gaming"]; // Array
+let person = {name: "Mugisha", age: 25}; // Object
+console.log(name, age, isStudent, hobbies, person);*/
+
+//QUIZ on variables in JS
+
+/*let favoritefood = 'Pizza';
+let birthyear = '2003';
+let myskills = 'JavaScript, C++, Python';
+
+console.log('My Favorite Food is:',favoritefood,);
+console.log('My Birthyear is:',birthyear);
+console.log('My skills are:',myskills);
+console.log("THANKS FOR YOUR TIME, GBU!");*/
+
+
+//Functions in JS
+//GREATINGS (non-parametarized function)
+/*function greatings() {
+console.log("Hello my dear", "WELCOME TO THE APPLICTON OF FUNCTIONS IN JS!");
+}
+
+greatings();*/
+
+//GREATINGS (parametarized function)
+
+/*function greatings(name) {
+console.log("Hello my dear," + name + "!");
+}
+
+greatings("MUGISHA");*/
+
+
+//Function with return statement
+
+/*function add(a, b) {
+    return a + b;
+}
+
+let sum = add(5, 10);
+console.log("The sum is:",sum);*/
+
+
+
