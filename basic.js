ef88733e10dcff32042a9589b1b6c711bee28f82
Mugisha24-pro <bazigajo24@gmail.com>
2025-11-22 12:55:47 +0200

    commented code examples in basic.js
    Comment out all variable declarations and function calls in basic.js.

--- basic.js
+++ basic.js
@@ -1,49 +1,49 @@
 //Variables in JS
 
-/*let name = "Mugisha";       // String
-let age = 25;               // Number
-let isStudent = true;       // Boolean            
-let hobbies = ["coding", "gaming"]; // Array
-let person = {name: "Mugisha", age: 25}; // Object
-console.log(name, age, isStudent, hobbies, person);*/
+// let name = "Mugisha";       // String
+// let age = 25;               // Number
+// let isStudent = true;       // Boolean            
+// let hobbies = ["coding", "gaming"]; // Array
+// let person = {name: "Mugisha", age: 25}; // Object
+// console.log(name, age, isStudent, hobbies, person);
 
 //QUIZ on variables in JS
 
-/*let favoritefood = 'Pizza';
-let birthyear = '2003';
-let myskills = 'JavaScript, C++, Python';
+// let favoritefood = 'Pizza';
+// let birthyear = '2003';
+// let myskills = 'JavaScript, C++, Python';
 
-console.log('My Favorite Food is:',favoritefood,);
-console.log('My Birthyear is:',birthyear);
-console.log('My skills are:',myskills);
-console.log("THANKS FOR YOUR TIME, GBU!");*/
+// console.log('My Favorite Food is:',favoritefood,);
+// console.log('My Birthyear is:',birthyear);
+// console.log('My skills are:',myskills);
+// console.log("THANKS FOR YOUR TIME, GBU!");
 
 
 //Functions in JS
 //GREATINGS (non-parametarized function)
-/*function greatings() {
-console.log("Hello my dear", "WELCOME TO THE APPLICTON OF FUNCTIONS IN JS!");
-}
+// function greatings() {
+// console.log("Hello my dear", "WELCOME TO THE APPLICTON OF FUNCTIONS IN JS!");
+// }
 
-greatings();*/
+// greatings();
 
 //GREATINGS (parametarized function)
 
-/*function greatings(name) {
-console.log("Hello my dear," + name + "!");
-}
+// function greatings(name) {
+// console.log("Hello my dear," + name + "!");
+// }
 
-greatings("MUGISHA");*/
+// greatings("MUGISHA");
 
 
 //Function with return statement
 
-/*function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
 
-let sum = add(5, 10);
-console.log("The sum is:",sum);*/
+// let sum = add(5, 10);
+// console.log("The sum is:",sum);
 
 
 
